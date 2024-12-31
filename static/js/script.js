31d5912d720d6ad0dfca65e37fe0422daacb4bca
Gylmynnn <muhamadgilman999@gmail.com>
2024-12-31 14:39:23 +0800

    core : Javascript script template

---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+const imageInput = document.getElementById("imageInput");
+const imagePreviewContainer = document.getElementById("imagePreviewContainer");
+
+imageInput.addEventListener("change", function (event) {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const imgElement = document.createElement("img");
+      imgElement.src = e.target.result;
+      imgElement.classList.add("image-preview");
+      imagePreviewContainer.innerHTML = ""; // Clear previous preview
+      imagePreviewContainer.appendChild(imgElement);
+    };
+    reader.readAsDataURL(file);
+  }
+});
