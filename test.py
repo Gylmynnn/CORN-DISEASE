dad8414321440c74cad1720563780ba2e258a136
Gylmynnn <muhamadgilman999@gmail.com>
2025-01-03 18:04:33 +0800

    feat : add plotlib diagram

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request, url_for
-from keras._tf_keras.keras.models import load_model
+# from keras._tf_keras.keras.models import load_model
+# from matplotlib import pyplot as plt
+# from tensorflow.python.keras.models import load_model
+# from tensorflow.keras.models import load_model, save_model
+from tensorflow.python.keras.models import load_model
 import numpy as np
 from PIL import Image
 import os
 
 # Membuat aplikasi Flask
 app = Flask(__name__)
+
 
 # Atur folder untuk file statis
 app.config['UPLOAD_FOLDER'] = 'static/images'
@@ -36,7 +41,7 @@ def preprocess_image(img, save_intermediate=False, filename=""):
     img_array = np.expand_dims(img_array, axis=0)
 
     # Return gambar yang diproses dan jalur file
-    return img_array, resized_path, normalized_path
+    return img_array
 
 
 # Rute untuk halaman utama
@@ -48,12 +53,12 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'file' not in request.files:
-        return render_template('index.html', error='No file part')
+        return render_template('index.html', error='Bukan bertipe file')
 
     file = request.files['file']
 
     if file.filename == '':
-        return render_template('index.html', error='No selected file')
+        return render_template('index.html', error='Tidak ada gambar yang dipilih')
 
     try:
         img = Image.open(file.stream)
@@ -67,6 +72,23 @@ def predict():
         predicted_class = class_labels[np.argmax(predictions)]
         confidence = np.max(predictions) * 100  # Convert to percentage
 
+
+        # # Buat diagram batang
+        # plt.figure(figsize=(8, 6))
+        # plt.bar(class_labels, predictions * 100, color='skyblue')
+        # plt.title('Hasil Prediksi')
+        # plt.xlabel('Kelas')
+        # plt.ylabel('Probabilitas (%)')
+        # plt.xticks(rotation=45)
+
+        # # Simpan diagram ke dalam folder statis
+        # chart_path = os.path.join(app.config['UPLOAD_FOLDER'], f"{filename}_chart.png")
+        # plt.savefig(chart_path)
+        # plt.close()
+
+        # # URL untuk diagram
+        # chart_url = url_for('static', filename=f'images/{filename}_chart.png')
+
         # Kirim hasil prediksi dan URL gambar ke template
         resized_url = url_for('static', filename=f'images/{filename}_resized.jpg')
         normalized_url = url_for('static', filename=f'images/{filename}_normalized.jpg')
@@ -76,7 +98,8 @@ def predict():
             prediction=predicted_class,
             confidence=confidence,
             resized_url=resized_url,
-            normalized_url=normalized_url
+            normalized_url=normalized_url,
+            # chart_url=chart_url
         )
 
     except Exception as e:
